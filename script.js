a14574361e7aeb7d2e99257871c2acb0a3bd1c4d
mermadly <34113249+mermadly@users.noreply.github.com>
2019-07-05 14:05:37 -0300

    First try

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('carousel').slick({
+    });
+  });
+        
+$('.multiple-items').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 2
+  });
+      
+  
